40fd443d62b28fe7b1aec21f24a231cd0239a9c3
cormac butler <cbutler20@qub.ac.uk>
2025-02-16 21:36:46 +0000

    textfile storage for faster graph access

---
+++ Extension System - Traffic/TrafficLightClass.py
@@ -0,0 +1,19 @@
+from VehicleClass import VehicleClass as vc
+
+class TrafficLightClass:
+    def __init__(self, position, green_duration, orange_duration, red_duration):
+        self.position = position
+        self.green_duration = green_duration
+        self.orange_duration = orange_duration
+        self.red_duration = red_duration
+
+    def status(self, time):
+        cycle_time = self.green_duration + self.orange_duration + self.red_duration
+        phase = time % cycle_time
+
+        if phase < self.green_duration:
+            return "green"
+        elif phase < self.green_duration + self.orange_duration:
+            return "orange"
+        else:
+            return "red"
